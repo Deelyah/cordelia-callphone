f7209b9f394307bfcb416cb1f87aced1bb4efc9a
PraiseDelia <elemdelia@gmail>
2022-08-31 18:22:54 +0100

    add: protected route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,9 @@
 import { Navigate } from "react-router-dom";
-
+import { Outlet } from "react-router-dom";
 const ProtectedRoute = () => {
   let isAuthenticated = window.localStorage.getItem("IS_AUTHENTICATED");
   return (
-    <>
-      {isAuthenticated ? (
-        <Navigate to="/home"></Navigate>
-      ) : (
-        <Navigate to="/login"></Navigate>
-      )}
-    </>
+    <>{isAuthenticated ? <Outlet /> : <Navigate to="/login"></Navigate>}</>
   );
 };
 export default ProtectedRoute;
